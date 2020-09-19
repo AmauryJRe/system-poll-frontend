8e520da27d3b89b5d63ff9e7c80c9139f7f86e29
amauryjr <amauryjreynaldo@gmail.com>
2020-09-18 21:52:00 -0400

    Handle Poll options

--- src/components/Polls/Poll/PollForm.js
+++ src/components/Polls/Poll/PollForm.js
@@ -1,75 +1,79 @@
-import React, { Component } from "react";
-import { Button, Card, Form, FormControl } from "react-bootstrap";
+import React, { Component, useState } from "react";
+import { Button, Card, Form, FormControl, InputGroup } from "react-bootstrap";
 
-export default class PollForm extends Component {
-	state = {
-		options: [{ name: "" }],
+function PollForm() {
+
+	const [options, setOptions] = useState([{name:""}]);
+
+	const handleInputChange = (e, index) => {
+		const { name, value } = e.target;
+		const list = [...options];
+		list[index][name] = value;
+		setOptions(list);
+		console.log(list);
 	};
 
+	const handleRemoveClick = (index) => {
+		const list = [...options];
+		list.splice(index, 1);
+		setOptions(list);
+	};
 
-	render() {
+	const handleAddClick = (event) => {
+		setOptions([...options, { name: "" }]);
+		event.preventDefault();
+	};
 
-		let { options } = this.state;
-		
-		const handleInputChange = (e, index) => {
-			const { name, value } = e.target;
-			const list = [...options];
-			list[index][name] = value;
-			this.setState({
-				options: list,
-			});
-
-			console.log(list);
-		};
-
-		const handleRemoveClick = (index) => {
-			const list = [...options];
-			list.splice(index, 1);
-			this.setState({
-				options: list,
-			});
-		};
-
-		const handleAddClick = () => {
-			this.setState({
-				options: { name: "" },
-			});
-		};
-
-		return (
-			<div class="container">
-				<Card border="success">
-					<Card.Header as="h5" className="text-center">
-						Featured
-					</Card.Header>
-					<Card.Body>
-						<Card.Title>Special title treatment</Card.Title>
-						<Form>
-							<Form.Group controlId="formGroupName">
-								<Form.Label>Poll Name</Form.Label>
-								<Form.Control type="text" placeholder="Enter The poll name" defaultValue="asdads" />
-							</Form.Group>
-							<Form.Group controlId="formGroupOptions">
-								<Form.Label>AddOption</Form.Label>
-								{options.map((opt, idx) => {
-									return (
+	return (
+		<div class="container">
+			<Card border="success">
+				<Card.Header as="h5" className="text-center">
+					Featured
+				</Card.Header>
+				<Card.Body>
+					<Card.Title>Special title treatment</Card.Title>
+					<Form>
+						<Form.Group controlId="formGroupName">
+							<Form.Label>Poll Name</Form.Label>
+							<Form.Control type="text" placeholder="Enter The poll name" defaultValue="asdads" />
+						</Form.Group>
+						<Form.Group controlId="formGroupOptions">
+							<Form.Label>AddOption</Form.Label>
+							{options.map((opt, idx) => {
+								return (
+									<InputGroup className="mb-3">
 										<FormControl
 											name="optionName"
 											placeholder="Add option here"
 											defaultValue="asd"
 											onChange={(e) => handleInputChange(e, idx)}
 										/>
-									);
-								})}
-							</Form.Group>
+										<InputGroup.Append>
+										{ options.length !== 1 &&
+											<button variant="outline-secondary" onClick={handleRemoveClick}>
+												Remove
+											</button>
+										}
 
-							<Button type="submit" variant="primary">
-								Go somewhere
-							</Button>
-						</Form>
-					</Card.Body>
-				</Card>
-			</div>
-		);
-	}
+{ options.length -1 === idx &&
+											<button variant="outline-secondary" onClick={(e) => handleAddClick(e)}>
+												Add
+											</button>
+										}
+										</InputGroup.Append>
+									</InputGroup>
+								);
+							})}
+						</Form.Group>
+
+						<Button type="submit" variant="primary">
+							Go somewhere
+						</Button>
+					</Form>
+				</Card.Body>
+			</Card>
+		</div>
+	);
 }
+
+export default PollForm;
