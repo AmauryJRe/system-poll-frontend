7bd63aceaba8a7d336838f58b13c283313b6165a
Amaury Jorge Reynaldo <amauryjreynaldo@gmail.com>
2020-09-19 16:10:32 -0400

    Add registration and login route

--- src/components/Router/Router.js
+++ src/components/Router/Router.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from "../Header/Header";
 import PollForm from "../Polls/Poll/PollForm";
+import RegistrationForm from '../auth/RegistrationForm';
+import LoginForm from '../auth/LoginForm';
 import Polls from "../Polls/Polls";
 
 export default class Router extends Component {
@@ -13,6 +15,8 @@ export default class Router extends Component {
 					<Switch>
 						<Route exact path="/" render={() => <Polls polls={this.props.polls} />} />
 						<Route exact path="/addpoll" render={() => <PollForm />} />
+						<Route exact path="/register" render={() => <RegistrationForm />} />
+						<Route exact path="/login" render={() => <LoginForm />} />
 					</Switch>
 				</div>
 			</BrowserRouter>
