4abc074e2685016ba91ba619e64ced486d16a56a
Amaury Jorge Reynaldo <amauryjreynaldo@gmail.com>
2020-09-20 21:46:53 -0400

    Add sweet alert notification

--- src/components/auth/RegistrationForm.jsx
+++ src/components/auth/RegistrationForm.jsx
@@ -1,57 +1,69 @@
-import React, { Component, useState } from "react";
-import { Button, Card, Form, FormControl, InputGroup, Row, Col } from "react-bootstrap";
-const axios = require('axios');
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { Button, Form } from "react-bootstrap";
+import SweetAlert from 'react-bootstrap-sweetalert';
+import axios from 'axios';
 
 function RegistrationForm() {
-	const [options, setOptions] = useState();
+	const [formData, setOptions] = useState({});
+	const [show, setShow] = useState(false);
+	const [errors, setErrors] = useState('')
+	const [showError,setShowError] = useState(false)
+	let history = useHistory();
 
 	const sendDataToApi = (e) => {
 		e.preventDefault();
 		console.log('Submit register form');
-		// let optionsFiltered = options.filter((option) => {
-		// 	return option.name !== "";
-		// });
-
-		// let optionsFinal = {};
-
-		// optionsFiltered.forEach((element) => {
-		// 	optionsFinal[element.name] = 0;
-		// });
-
-		// console.log({
-		// 	name: pollName.current.value,
-		// 	options: JSON.stringify(optionsFinal),
-		// });
-
-		// axios.post("http://localhost:5000/user/register", {
-		// 	name: pollName.current.value,
-		// 	options: JSON.stringify(optionsFinal)
-		// }).then(response=>{
-		// 	console.log(response)
-		// }).catch(err=>{
-		// 	console.log(err)
-		// });
+		console.log(formData)
+		axios.post("http://localhost:5000/user/register", formData ).then(response=>{
+			console.log(response)
+			//TODO Store the user data in the sate variable. Add in the setState function the function to store to the local storage too
+			setShow(true)
+		}).catch(err => {
+			const { error } = err.response.data
+			console.log(error)
+			setShowError(true)
+			console.log(err)
+			setErrors(error)
+		});
 	};
 
 	const handleInputChange = (e) => {
 		let { name, value } = e.target;
-		console.log('Input:',name+'=', value)
-
-    };
+		formData.[name] = value;
+		setOptions(formData);
+	};
+	
+	const onConfirm = () => { 
+		setShow(false)
+		history.push("/");
+	}
+	const onConfirmError = ()=>{ 
+		setShowError(false)
+		// TODO mark the field
+	}
 
 	return (
 		<div className="container col-md-6">
 			<Form onSubmit={(e) => sendDataToApi(e)}>
+				
 				<Form.Group controlId="exampleForm.ControlInput">
 					<Form.Label>Full Name</Form.Label>
 					<Form.Control type="text" name="fullName" placeholder="Full Name" onChange={(e) => handleInputChange(e)} />
 				</Form.Group>
-				<Form.Group controlId="exampleForm.ControlInput">
+				<Form.Group controlId="exampleForm.ControlInput" >
 					<Form.Label>Username</Form.Label>
-					<Form.Control type="username" name="username" placeholder="username" onChange={(e) => handleInputChange(e)}/>
+					<Form.Control isInvalid={!!errors} type="username" name="username" placeholder="username" onChange={(e) => handleInputChange(e)}/>
+				<Form.Control.Feedback type="invalid">
+                {errors}
+        </Form.Control.Feedback>
 				</Form.Group>
 				<Form.Group controlId="formPlaintextPassword">
 					<Form.Label>Password</Form.Label>
+						<Form.Control type="password" name="password" placeholder="Password" onChange={(e) => handleInputChange(e)}/>
+				</Form.Group>
+				<Form.Group controlId="formPlaintextPassword">
+					<Form.Label>Repeat Password</Form.Label>
 						<Form.Control type="password" name="password" placeholder="Password" onChange={(e) => handleInputChange(e)}/>
 				</Form.Group>
 				<Form.Group>
@@ -69,8 +81,18 @@ function RegistrationForm() {
             />
           </Form.Group>
 				 <Button variant="primary" type="submit">
-          Submit
+					Submit
         </Button>
+				{show &&(
+					<SweetAlert success title={formData.fullName}	timeout={3500} onConfirm={()=>onConfirm()}>
+							You are successfully registered
+					</SweetAlert>
+				)}
+				{showError &&(
+					<SweetAlert error title='Error'	timeout={3500} onConfirm={()=>onConfirmError()}>
+						{errors}
+					</SweetAlert>
+				)}
 			</Form>
 		</div>
 	);
