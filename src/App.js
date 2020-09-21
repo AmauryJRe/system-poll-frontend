4f0084ca45e671c04683d40366e4f626d2513fc1
Amaury Jorge Reynaldo <amauryjreynaldo@gmail.com>
2020-09-21 14:36:57 -0400

    Add auth state to the main application

--- src/App.js
+++ src/App.js
@@ -2,10 +2,28 @@ import React, { Component } from "react";
 import Router from "./components/Router/Router";
 
 export default class App extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+					token: localStorage.getItem('polls.token'),
+					isLoggedIn: localStorage.getItem('polls.isLoggedIn'),
+					username: localStorage.getItem('polls.username'),
+					role: localStorage.getItem('polls.role')
+		};
+		this.setAuthState = (authState)=>{ 
+			this.setState(authState);
+			localStorage.setItem('polls.token',authState.token);
+			localStorage.setItem('polls.isLoggedIn',authState.isLoggedIn);
+			localStorage.setItem('polls.username',authState.username);
+			localStorage.setItem('polls.role',authState.role);
+		}
+  }
+
 	render() {
 		return (
 			<React.Fragment>
-				<Router/>
+				<Router auth={this.state} setAuthState={this.setAuthState}/>
 			</React.Fragment>
 		);
 	}
