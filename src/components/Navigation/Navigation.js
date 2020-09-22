ab5514c1eaf9f18a3174cf01b7942b257944d7a7
Amaury Jorge Reynaldo <amauryjreynaldo@gmail.com>
2020-09-21 23:54:56 -0400

    Refactoring component to function

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,11 +1,70 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { Component } from "react";
-import { Nav, Navbar } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import Sidebar from "../Sidebar/Sidebar";
 
-export default class Navigation extends Component {
-	render() {
+import React, { Fragment,useState } from "react";
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
+import Sidebar from "../Sidebar/Sidebar";
+import { Link, useHistory } from "react-router-dom";
+import SweetAlert from 'react-bootstrap-sweetalert';
+
+
+const Navigation=(props)=> {
+	
+	const { isLoggedIn } = props.auth;
+	const {setAuthState} = props;
+	const history = useHistory();
+	const [show, setShow] = useState(false);
+	const [username, setUsername] = useState()
+	const [showError,setShowError] = useState(false)
+
+	const handleClickLogOut = () => {
+		setUsername(props.auth.username)
+		setAuthState(null)
+		setShow(true)
+		
+	}
+
+	const onConfirm = () => { 
+		setShow(false)
+		history.push("/");
+	}
+	const onConfirmError = ()=>{ 
+		setShowError(false)
+		
+	}
+	const guestLinks = (
+			<Fragment>
+			<Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+			<Navbar.Toggle aria-controls="basic-navbar-nav" />
+			<Navbar.Collapse id="basic-navbar-nav">
+				<Nav className="mr-auto">
+					<Nav.Link as={Link} to={'/'}>Home</Nav.Link>
+				</Nav>
+			</Navbar.Collapse>
+			<Nav.Link className='mr-2 btn btn-sm btn-outline-success' as={Link} to={'/register'}>Register</Nav.Link>
+				<Nav.Link className='mr-2 btn btn-sm btn-outline-success' as={Link} to={'/login'}>Login</Nav.Link>
+			</Fragment>
+		);
+		const userLinks = (
+			<Fragment>
+			<Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+			<Navbar.Toggle aria-controls="basic-navbar-nav" />
+			<Navbar.Collapse id="basic-navbar-nav">
+				<Nav className="mr-auto">
+					<Nav.Link as={Link} to={'/'}>Home</Nav.Link>
+					<Nav.Link as={Link} to={'/addpoll'}>AddPoll</Nav.Link>
+					<NavDropdown title="Dropdown" id="basic-nav-dropdown">
+						<NavDropdown.Item as={Link} to={'/'}>Action</NavDropdown.Item>
+						<NavDropdown.Item as={Link} to={'/addpoll'}>Another action</NavDropdown.Item>
+						<NavDropdown.Item as={Link} to={'/'}>Something</NavDropdown.Item>
+						<NavDropdown.Divider />
+						<NavDropdown.Item as={Link} to={'/addpoll'}>Separated link</NavDropdown.Item>
+					</NavDropdown>
+				</Nav>
+			</Navbar.Collapse>
+				<Nav.Link className='mr-2 btn btn-sm btn-outline-success' onClick={handleClickLogOut}>Logout</Nav.Link>
+			</Fragment>
+		);
+		
 		return (
 			<Navbar bg="light" variant="light" expand="lg" sticky="top">
 				<Sidebar
@@ -29,16 +88,24 @@ export default class Navigation extends Component {
 						)}
 					</Nav>
 				</Navbar.Collapse>
-				{!localStorage.getItem("polls.isLoggedIn") && (
-						<Nav.Link className="mr-2 btn btn-sm btn-outline-success" as={Link} to={"/register"}>
-							Register
-						</Nav.Link>
-					) && (
-						<Nav.Link className="mr-2 btn btn-sm btn-outline-success" as={Link} to={"/login"}>
-							Login
-						</Nav.Link>
-					)}
+				<Nav.Link className="mr-2 btn btn-sm btn-outline-success" as={Link} to={"/register"}>
+					Register
+				</Nav.Link>
+				<Nav.Link className="mr-2 btn btn-sm btn-outline-success" as={Link} to={"/login"}>
+					Login
+				</Nav.Link>
+				{isLoggedIn ? userLinks: guestLinks}
+			{show &&(
+					<SweetAlert success title={username}	timeout={1700} onConfirm={()=>onConfirm()}>
+							You are logged out successfully
+					</SweetAlert>
+				)}
+				{showError &&(
+					<SweetAlert error title='Error'	timeout={3500} onConfirm={()=>onConfirmError()}>
+						{/* "{errors}" */}
+					</SweetAlert>
+				)}
 			</Navbar>
 		);
-	}
 }
+export default Navigation;
