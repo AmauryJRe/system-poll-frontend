b23fad9b72d74f4236cd5573cd248d146bc62a57
Amaury Jorge Reynaldo <amauryjreynaldo@gmail.com>
2020-09-22 00:00:11 -0400

    fix error

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,12 +6,9 @@ export default class Header extends Component {
 		return (
 			<header>
 				<Navigation
-					setSideBarVisible={this.props.setSideBarVisible}
-					setVisible={this.props.setVisible}
-					currentPoll={this.props.currentPoll}
-					makeVote={this.props.makeVote}
-					pollsUserCantVote={this.props.pollsUserCantVote}
 					title={this.props.title}
+					auth={this.props.auth}
+					setAuthState={this.props.setAuthState}
 				/>
 			</header>
 		);
